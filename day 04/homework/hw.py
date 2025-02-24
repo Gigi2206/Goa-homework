2828bd7ef86d5934d27b3a416d1546c52bd11cc2
Gigi2206 <gigidzirkvadze2009>
2025-02-24 22:32:35 +0400

    second commit

---
+++ day 04/homework/hw.py
@@ -0,0 +1,12 @@
+father = "davit"
+
+mother = "cira"
+
+brother ="demetre"
+
+sister = "არ მყავს"
+
+grandfather = "vaja"
+
+print("i am " + "gigi" + " this is my family " + " my father " + father + "," + " mother " + mother + "," " brother " + brother + "," + " grandfather " + grandfather )
+
