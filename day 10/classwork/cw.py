00b1fd1785d8c5b764a9b6d56f355dfad30c629b
Gigi2206 <gigidzirkvadze2009>
2025-03-13 23:58:47 +0400

    second commit

---
+++ day 10/classwork/cw.py
@@ -0,0 +1,9 @@
+name = "gigi"   # Name
+age = 15 # age
+height = 1.83
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
---
+++ day 10/homework/hw1.py
@@ -0,0 +1,21 @@
+"""თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი"""
+#string ცვლადები
+name = "gigi"
+surname = "dzirkvadze"
+hobby = "gaming"
+location = "batumi"
+value ="deme"
+
+#integer ცვლადები
+age = 15
+number = 30
+numbera = 20
+number2 = 15
+number3 = 25
+
+#float ცვლადები
+height = 1,84
+wona = 75
+number4 = 32.8
+number5 = 98.9
+number6 = 223.9
