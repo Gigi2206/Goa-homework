c9b4777f47a43ffa92214a787596c1e8c3d4298c
Gigi2206 <gigidzirkvadze2009>
2025-05-12 00:51:16 +0400

    second commit

---
+++ day 22/homework/hw4.py
@@ -0,0 +1,9 @@
+#5) ახსენით რა არის slicing
+
+my_list = [0, 1, 2, 3, 4, 5]
+sub_list = my_list[2:5]  # slicing: ინდექსიდან 2-დან 5-მდე (5-ის ჩათვლით) ელემენტები
+print(sub_list)  # -> [2, 3, 4]
+
+my_string = "გამარჯობა"
+sub_string = my_string[1:4] # slicing: ინდექსიდან 1-დან 4-მდე (4-ის ჩათვლით) სიმბოლოები
+print(sub_string) # -> "ამა"
